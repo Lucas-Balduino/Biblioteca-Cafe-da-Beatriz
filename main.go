89ceab93bf1b206249420b1bd35d9cf76f5d8fa5
Lucas-Balduino <165683227+Lucas-Balduino@users.noreply.github.com>
2024-06-04 12:31:41 -0300

    Add files via upload

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+import (
+	"aplicacao-web/routes"
+	"net/http"
+)
+
+func main(){
+	routes.Rotas()
+	http.ListenAndServe("https://lucas-balduino.github.io/Site-Lucas-Monitoria-HTML/", nil)
+}
