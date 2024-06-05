01ea0bd82de001f99c70b6c17a48304be4345967
awangelo <anggg.gaio@gmail.com>
2024-06-05 12:23:45 -0300

    fix port

--- back/main.go
+++ back/main.go
@@ -7,9 +7,5 @@ import (
 
 func main() {
 	routes.Rotas()
-<<<<<<< HEAD
 	http.ListenAndServe(":8000", nil)
-=======
-	http.ListenAndServe("https://lucas-balduino.github.io/Biblioteca-Cafe-da-Beatriz/", nil)
->>>>>>> 5ff102846444301328054c920e11b2b39d602fac
 }
